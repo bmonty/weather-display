baa22ff9fc85f9b9ee92ade6e1452336d85f6557
Benjamin Montgomery <bmontgom@montynet.org>
2018-01-15 22:15:24 -0600

    major package rewrite

--- weather-display.js
+++ weather-display.js
@@ -1,41 +1,92 @@
-const Lcd = require('lcd');
 const readline = require('readline');
 
-const lcd = new Lcd({
-    rs: 24,
-    e: 25,
-    data: [17, 18, 22, 23],
-    cols: 16,
-    rows: 2
-});
+const rpio = require('rpio');
+const hd44780 = require('gpio-hd44780-driver');
 
-lcd.on('ready', () => {
-    lcd.clear(() => {
-        const rl = readline.createInterface({
-            input: process.stdin
-        });
+const displayTypes = {
+    date: 1,
+    temp: 2,
+};
 
-        rl.on('line', (input) => {
-            const data = JSON.parse(input);
-
-            lcd.clear(() => {
-                lcd.setCursor(0, 0);
-                lcd.print(`Temp: ${data.temperature_F}F`, (err) => {
-                    if (err) throw err;
+const weatherDisplay = () => {
+    var tempData = 'Temp: not updated\nHumidity: not updated';
+    var display = displayTypes.temp;
+    var timeUpdate = false;
     
-                    lcd.setCursor(0, 1);
-                    lcd.print(`Humidity: ${data.humidity}%`, (err) => {
-                        if (err) throw err;
-                    });
-                });
- 
-            });
-       });
-    });
-});
+    rpio.open(26, rpio.OUTPUT, rpio.LOW);
 
-process.on('SIGINT', () => {
-    lcd.close();
-    process.exit();
-});
+    const lcd = new hd44780({
+        pinRs: 18,
+        pinEnable: 22,
+        pinsData: [11, 12, 15, 16],
+        lcdColumns: 16,
+        lcdRows: 2
+    });
+
+    const rl = readline.createInterface({
+        input: process.stdin
+    });
+
+    // update temperature data when a new line is read
+    rl.on('line', (input) => {
+        // update data
+        const data = JSON.parse(input);
+        tempData = `Temp: ${data.temperature_F}F\nHumidity: ${data.humidity}%`;
+
+        // pulse LED
+        rpio.write(26, rpio.HIGH);
+        setTimeout(() => {
+            rpio.write(26, rpio.LOW);
+        }, 1500);
+    });
+
+    // initial display
+    dateTimeDisplay();
+    timeUpdate = setInterval(dateTimeDisplay, 1000);
+
+    // update display every 5 seconds
+    const displayInterval = setInterval(() => {
+        if (timeUpdate) {
+            clearInterval(timeUpdate);
+            timeUpdate = false;
+        }
+
+        lcd.clear();
+
+        if (display === displayTypes.temp) {
+            lcd.print(tempData);
+            display = displayTypes.date;
+            return;
+        }
+
+        if (display === displayTypes.date) {
+            // do initial display update
+            dateTimeDisplay();
+
+            // update the date/time every second
+            timeUpdate = setInterval(dateTimeDisplay, 1000);
+
+            display = displayTypes.temp;
+            return;
+        }
+    }, 5000);
+
+    // cleanup and close the program on SIGINT
+    process.on('SIGINT', () => {
+        rl.close();
+        clearInterval(displayInterval);
+        console.log('Done!');
+        process.exit();
+    });
+
+    function dateTimeDisplay() {
+        lcd.setCursor(0, 0);
+
+        // print the date and time on the display
+        const now = new Date();
+        lcd.print(`${now.toDateString()}\n${now.toTimeString().slice(0, 8)}`);
+    }
+};
+
+weatherDisplay();
 
